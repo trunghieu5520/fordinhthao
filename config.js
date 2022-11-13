aefeb44b749426e665b97b94a300c5f36393aaf0
Nguyễn Trung Hieu <78343616+trunghieu5520@users.noreply.github.com>
2022-11-13 23:14:37 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    introTitle: 'Chào DinhThao!',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: 'Click vào đây',
+    title: 'Bạn sẽ dẫn tui đi ăn chứ?',
+    desc: 'Hoặc đi lượn phố cũng được =))))',
+    btnYes: 'Tớ đồng ý <3',
+    btnNo: 'Không đâuuuu',
+    question:'Tại sao b đồng vậyyyy :>',
+    btnReply: 'Gửi cho tui <3',
+    reply: 'Vì anhh rất đẹp trai <33333333',
+    mess: 'Xin cam onnn :>>>',
+    messDesc: 'Cuối tuần đii luon nheee :>',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.facebook.com/hieu.nguyen.5520/'
+}
